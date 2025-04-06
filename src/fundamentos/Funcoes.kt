5e4efea56f23e014bcf1194354d22cdc1290e719
peerezjoao <peerezjoao1@gmail.com>
2025-04-06 20:55:23 -0300

    feat(fundamentos/Funcoes) adicionando novas funções

--- src/fundamentos/Funcoes.kt
+++ src/fundamentos/Funcoes.kt
@@ -1,10 +1,33 @@
 package fundamentos
 
+val usuarios: MutableList<String> = mutableListOf<String>("joao_p", "giullia_v")
+
 fun main() {
-    println(boasVindas("James"))
+   // println(boasVindas("James"))
+   // println(mensagemSufixoPrefixo("bem vindo", "welcome"))
+    validarUsuario("joao_q")
 }
 
 fun boasVindas(nome: String) : String {
     var mensagemBemVindo: String = "$nome, bem vindo ao Google!"
     return mensagemBemVindo
+
+}
+
+fun mensagemSufixoPrefixo(sufixo: String, prefixo: String): String {
+    return "$prefixo $sufixo"
+}
+
+fun validarUsuario(usuario: String): String  {
+
+    if (usuario in usuarios) {
+        println("Usuário já cadastrado na plataforma.")
+        return "Usuário já cadastrado."
+    } else {
+        usuarios.add(usuario)
+        println(usuarios)
+        return "Usuário - $usuario  - adicionado com sucesos na base."
+    }
+
+
 }
