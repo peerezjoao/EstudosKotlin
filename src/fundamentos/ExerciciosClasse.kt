3260d76530ecbd5bf4e41b72b93bd1b2912813bb
peerezjoao <peerezjoao1@gmail.com>
2025-04-07 19:17:37 -0300

    feat(fundamentos/Classes) classes exercicios

--- src/fundamentos/ExerciciosClasse.kt
+++ src/fundamentos/ExerciciosClasse.kt
@@ -4,7 +4,6 @@ package fundamentos
 Make sure that the property for salary is mutable, otherwise you won’t get a salary boost at the end of the year!
 The main function demonstrates how you can use this data class.
 */
-
 class Employee(val name: String, var salary:Int) {
 
     fun showData() {
